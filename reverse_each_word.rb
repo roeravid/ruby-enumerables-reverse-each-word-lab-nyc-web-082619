34463943276102a23a16686a726a5a200e5ec8a5
Joe David <javid92@gmail.com>
2019-08-24 23:45:48 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,15 @@
 def reverse_each_word(string = "")
-  arrayFromString = string.split
-  arrayFromString.reverse
+  splitString = string.split("")
+  backwards = []
+  string.size.times {backwards << splitString.pop}
+  backwardsSameIndex = backwards.join
 
-  arrayFromString
+  backwardsSameIndex
+end
+
+def reverse_each_word(string="")
+  split_string = string.split("")
+  reversed = []
+  string.size.times { reversed << split_string.pop }
+  reversed.join
 end
