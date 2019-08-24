812e7019a680a736a21b8cf431c4674d3f04f554
Joe David <javid92@gmail.com>
2019-08-24 23:59:21 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,15 +1,11 @@
-def reverse_each_word(string = "")
-  splitString = string.split("")
-  backwards = []
-  string.size.times {backwards << splitString.pop}
-  backwardsSameIndex = backwards.join
-
-  backwardsSameIndex
+def reverse_each_word(string = "") 
+  arrayFromString = string.split
+  arrayFromString  
 end
 
-def reverse_each_word(string="")
-  split_string = string.split("")
-  reversed = []
-  string.size.times { reversed << split_string.pop }
-  reversed.join
-end
+
+#take a string
+#separate the words into an array of strings (.split)
+# go thru each word, reverse the order of the letters,
+#   and copy to a new array called 'finalarray',
+#   letters reversed but words the same order
