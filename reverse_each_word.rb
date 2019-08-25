bdb70db68e2d1b732dad179d3cb46c62ea51c383
Joe David <javid92@gmail.com>
2019-08-25 00:45:45 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,18 +1,25 @@
-def reverse_each_word(sentence1, sentence2)
-  arrayFromString = string.split
-  reversedArray = []
-  counter = 0
-  while counter <= arrayFromString.length do
-    reversedArray.unshift(arrayFromString[counter])
-    arrayFromString[counter].reverse
-    counter +=1
-  end
-reversedArray
+def reverse_each_word(string)
+  
 end
+
+
+
+# def reverse_each_word(string)
+#   arrayFromString = string.split
+#   reversedArray = []
+#   counter = 0
+#   while counter <= arrayFromString.length do
+#     reversedArray.unshift(arrayFromString[counter])
+#     arrayFromString[counter].reverse
+#     counter +=1
+#   end
+# reversedArray
+# end
 
 
 #take a string
 #separate the words into an array of strings (.split)
-# go thru each word, reverse the order of the letters,
+# go thru each word
+#   reverse the order of the letters,
 #   and copy to a new array called 'finalarray',
 #   letters reversed but words the same order
